73e421e00d6681dc561ac4519c115ce662639608
Dasha Gorodilova <dasha.gorodilowa@yandex.ru>
2023-12-19 22:58:52 +0300

    advice1

--- src/main/java/ru/yourbunny/yourbunny/models/Advice.java
+++ src/main/java/ru/yourbunny/yourbunny/models/Advice.java
@@ -1,2 +1,20 @@
-package ru.yourbunny.yourbunny.models;public class Advice {
+package ru.yourbunny.yourbunny.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "advice")
+@Data
+@NoArgsConstructor
+
+public class Advice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "advice_id")
+    private long id;
+    @Column(name = "advice", nullable = false)
+    private String advice;
+
 }
--- src/main/java/ru/yourbunny/yourbunny/repositories/AdviceRepository.java
+++ src/main/java/ru/yourbunny/yourbunny/repositories/AdviceRepository.java
@@ -1,2 +1,12 @@
-package ru.yourbunny.yourbunny.repositories;public interface AdviceRepository {
+package ru.yourbunny.yourbunny.repositories;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.yourbunny.yourbunny.models.Advice;
+import java.util.Optional;
+
+public interface AdviceRepository extends JpaRepository <Advice, Long> {
+    @Transactional
+    Optional<Advice> findById(long id);
+
 }
